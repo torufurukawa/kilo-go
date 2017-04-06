7219d2f3cbc536f8e3e52d6359e18a5256daf4db
Toru Furukawa <torufurukawa@gmail.com>
2017-04-07 07:51:59 +0900

    Add quitting

--- kilo.go
+++ kilo.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"os"
+)
 
+func main() {
+	b := make([]byte, 1)
+	for {
+		n, err := os.Stdin.Read(b)
+		// EOF or error
+		if n != 1 || err != nil {
+			break
+		}
+
+		// quit
+		if b[0] == 'q' {
+			break
+		}
+	}
 }
