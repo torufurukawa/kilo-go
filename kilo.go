8a7dbd9144e16ef1e762049c3bbdf1f3d8841879
Toru Furukawa <torufurukawa@gmail.com>
2017-04-07 08:11:40 +0900

    Add raw mode

--- kilo.go
+++ kilo.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"os"
+
+	"golang.org/x/crypto/ssh/terminal"
 )
 
+var state *terminal.State
+
 func main() {
+	err := enableRawMode()
+	if err != nil {
+		panic(err)
+	}
+	defer disableRawMode()
+
 	b := make([]byte, 1)
 	for {
 		n, err := os.Stdin.Read(b)
@@ -18,4 +28,14 @@ func main() {
 			break
 		}
 	}
+}
+
+func enableRawMode() error {
+	var err error
+	state, err = terminal.MakeRaw(0)
+	return err
+}
+
+func disableRawMode() {
+	terminal.Restore(0, state)
 }
