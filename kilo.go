a8b43cfa42edf3487f9e09ce363cf39a051da077
Toru Furukawa <torufurukawa@gmail.com>
2017-04-10 13:41:52 +0900

    Add drawing rows Change quit command key from Q to CTRL-Q

--- kilo.go
+++ kilo.go
@@ -1,62 +1,91 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 	"os"
-
-	"unicode"
 
 	"golang.org/x/crypto/ssh/terminal"
 )
 
+const ctrlQ byte = 'q' & 0x1f
+
 func main() {
-	err := enableRawMode()
+	enableRawMode()
+	defer disableRawMode()
+
+	for {
+		editorRefreshScreen()
+		if quit := editorProcessKeypress(); quit {
+			break
+		}
+	}
+}
+
+// terminal
+
+var originalState *terminal.State
+
+func enableRawMode() {
+	var err error
+	originalState, err = terminal.MakeRaw(0)
 	if err != nil {
 		die(err)
 	}
-	defer disableRawMode()
-
-	b := make([]byte, 1)
-	for {
-		n, err := os.Stdin.Read(b)
-		// EOF or error
-		if n != 1 || err != nil {
-			die(err)
-		}
-
-		// quit
-		if b[0] == 'q' {
-			break
-		}
-
-		// print
-		c := bytes.Runes(b)[0]
-		if unicode.IsControl(c) {
-			fmt.Printf("%d\r\n", c)
-		} else {
-			fmt.Printf("%d ('%c')\r\n", c, c)
-		}
-	}
-}
-
-//
-// terminal
-//
-
-var state *terminal.State
-
-func enableRawMode() error {
-	var err error
-	state, err = terminal.MakeRaw(0)
-	return err
 }
 
 func disableRawMode() {
-	terminal.Restore(0, state)
+	terminal.Restore(0, originalState)
 }
 
 func die(err error) {
+	os.Stdout.WriteString("\x1b[2J")
+	os.Stdout.WriteString("\x1b[H")
+
 	fmt.Fprintln(os.Stderr, err)
 	os.Exit(1)
+}
+
+func editorReadKey() byte {
+	b := make([]byte, 1)
+	n, err := os.Stdin.Read(b)
+	// EOF or error
+	if n != 1 || err != nil {
+		die(err)
+	}
+	return b[0]
+}
+
+// input
+
+func editorProcessKeypress() (quit bool) {
+	c := editorReadKey()
+	switch c {
+	case ctrlQ:
+		return true
+	}
+
+	return false
+}
+
+// output
+
+func editorRefreshScreen() {
+	os.Stdout.WriteString("\x1b[2J")
+	os.Stdout.WriteString("\x1b[H")
+	editorDrawRows()
+	os.Stdout.WriteString("\x1b[H")
+}
+
+func editorDrawRows() {
+	_, h, err := terminal.GetSize(0)
+	if err != nil {
+		die(err)
+	}
+	for y := 0; y < h; y++ {
+		os.Stdout.WriteString("~")
+
+		if y < h-1 {
+			os.Stdout.WriteString("\r\n")
+		}
+	}
 }
